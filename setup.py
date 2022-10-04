3ca071d02b677c95be76e2bd1e0e24fcceccd6e4
Henri Dwyer <henridwyer@users.noreply.github.com>
2022-10-04 17:02:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(),
+    packages=find_packages(include=['lambo*']),
 )
